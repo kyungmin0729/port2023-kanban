5dc8e400a88176f9b962fe174b64dc9e40b09091
kyungmin0729 <kyungmin0729@naver.com>
2023-11-19 20:02:24 +0900

    README.md 추가

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.kyungmin0729;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
